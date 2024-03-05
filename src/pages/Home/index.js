e5502522eb171a38ae76365594620ae126689788
Maxence Cool <max-game10@live.fr>
2024-03-05 13:53:06 +0100

    maj du last enlevé

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -114,13 +114,16 @@ const Page = () => {
       <footer className="row">
         <div className="col presta">
           <h3>Notre derniére prestation</h3>
-          <EventCard
-            imageSrc={last?.cover}
-            title={last?.title}
-            date={new Date(last?.date)}
-            small
-            label="boom"
-          />
+          {/* ajout de last en rendu conditionnel */}
+          {last && (
+            <EventCard
+              imageSrc={last?.cover}
+              title={last?.title}
+              date={new Date(last?.date)}
+              small
+              label="boom"
+            />
+          )}
         </div>
         <div className="col contact">
           <h3>Contactez-nous</h3>
